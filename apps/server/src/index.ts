bc77d4bca67b2e7549d2823fc333536a3ac0f24b
Your Name <the.jesse.dev@gmail.com>
2025-01-03 13:23:52 -0800

    cleaned up bun server code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,35 +1,29 @@
-const PORT = 3000;
-
-console.log(`Server running on port: ${PORT}`);
-
-Bun.serve({
-  port: PORT,
-  async fetch(req) {
-    return trpcHandler(req);
-  },
-});
-
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { createTRPCContext } from "./api/trpc";
 import { appRouter } from "./api/root";
+import { createTRPCContext } from "./api/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { serve } from "bun";
 
-const trpcHandler = async (req: Request): Promise<Response> => {
+// const PORT = parseInt(process.env.PORT || "3000", 10);
+const PORT = 4000;
+
+// The function that dispatches tRPC requests
+async function trpcHandler(req: Request) {
   const url = new URL(req.url);
 
-  // If the request is for a tRPC endpoint
   if (url.pathname.startsWith("/trpc")) {
-    // Delegate to the tRPC fetch handler
     return fetchRequestHandler({
       endpoint: "/trpc",
       req,
-      router: appRouter, // Use your appRouter instance
-      createContext: () =>
-        createTRPCContext({
-          headers: req.headers,
-        }), // Your context creation logic
+      router: appRouter,
+      createContext: () => createTRPCContext({ headers: req.headers }),
+      // onError, etc.
     });
   }
 
-  // For other routes, fallback to 404
-  return new Response("404!", { status: 404 });
-};
+  return new Response("Not found", { status: 404 });
+}
+
+serve({
+  port: PORT,
+  fetch: (req) => trpcHandler(req),
+});
