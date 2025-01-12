c1bf8493a80b91bc365ad44590a565b024ba8c5a
Your Name <the.jesse.dev@gmail.com>
2025-01-11 17:52:47 -0800

    emitter defintion

---
+++ apps/manipulator/src/api/common/emitter.ts
@@ -0,0 +1,25 @@
+import EventEmitter, { on } from "events";
+import type { ExtendedTaskObject } from "../routers/task/repository/task.repository.types";
+
+interface MyEvents {
+  // add: (orgId: string, data: TaskQuery) => void;
+  add: (task: ExtendedTaskObject) => void;
+}
+declare interface MyEventEmitter {
+  on<TEv extends keyof MyEvents>(event: TEv, listener: MyEvents[TEv]): this;
+  off<TEv extends keyof MyEvents>(event: TEv, listener: MyEvents[TEv]): this;
+  once<TEv extends keyof MyEvents>(event: TEv, listener: MyEvents[TEv]): this;
+  emit<TEv extends keyof MyEvents>(
+    event: TEv,
+    ...args: Parameters<MyEvents[TEv]>
+  ): boolean;
+}
+class MyEventEmitter extends EventEmitter {
+  public toIterable<TEv extends keyof MyEvents>(
+    event: TEv,
+    opts: NonNullable<Parameters<typeof on>[2]>
+  ): AsyncIterable<Parameters<MyEvents[TEv]>> {
+    return on(this, event, opts) as any;
+  }
+}
+export const ee = new MyEventEmitter(); // use redis if I have more than 1 user
